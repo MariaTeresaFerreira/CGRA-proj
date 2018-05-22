9d126f6a39a8fd880975c9b130562ee8c5c042da
Francisco Miranda <francisc03@hotmail.com>
2018-05-22 17:17:51 +0100

    Added wheel class and wheel and rim textures

---
+++ MyWheel.js
@@ -0,0 +1,77 @@
+/*/***
+ * MyWheel
+ * @constructor
+ */
+var degToRad = Math.PI / 180.0;
+
+
+class MyWheel extends CGFobject
+{
+
+	constructor(scene)
+	{
+		super(scene);
+		this.cylinder = new MyCylinder(scene, 12, 1);
+ 		this.circle = new MyCircle(scene, 20);
+
+ 		this.rimAppearance = new CGFappearance(this.scene);
+    	this.rimAppearance.setAmbient(0.5, 0.5, 0.5, 0.5);
+    	this.rimAppearance.setDiffuse(0.9, 0.9, 0.9, 0.5);
+    	this.rimAppearance.setSpecular(0.1, 0.1, 0.1, 0.5);
+    	this.rimAppearance.setShininess(20);
+    	this.rimAppearance.loadTexture("../resources/images/rims.png");
+
+
+ 		this.wheelAppearance = new CGFappearance(this.scene);
+    	this.wheelAppearance.setAmbient(0.5, 0.5, 0.5, 0.5);
+    	this.wheelAppearance.setDiffuse(0.9, 0.9, 0.9, 0.5);
+    	this.wheelAppearance.setSpecular(0.1, 0.1, 0.1, 0.5);
+    	this.wheelAppearance.setShininess(20);
+    	this.wheelAppearance.loadTexture("../resources/images/wheel.png");
+
+
+
+	    this.blankAppearance = new CGFappearance(this.scene);
+   		this.blankAppearance.setAmbient(0.0, 0.0, 0.0, 0.0);
+   	 	this.blankAppearance.setDiffuse(0.0, 0.0, 0.0, 0.0);
+    	this.blankAppearance.setSpecular(0.0, 0.0, 0.0, 0.0);
+    	this.blankAppearance.setShininess(0);
+		
+	};
+	
+	display() {
+
+ 	var degToRad = Math.PI / 180.0;
+	
+	//front of the wheel
+	this.scene.pushMatrix();
+		this.scene.scale(1, 1, 0.5);
+		this.wheelAppearance.apply();
+		this.cylinder.display();
+	this.scene.popMatrix();
+
+	//side of the wheel
+	this.scene.pushMatrix();
+		this.scene.translate(0,0,0.5);
+		this.rimAppearance.apply();
+		this.circle.display();
+	this.scene.popMatrix();
+	
+	//other side of the wheel
+	this.scene.pushMatrix();
+		this.scene.rotate(180 * degToRad,0,1,0)
+		//this.scene.translate(0,0,0.5);
+		this.rimAppearance.apply();
+		this.circle.display();
+	this.scene.popMatrix();
+	};   
+
+ };
+ 
+
+
+
+ 
+
+
+
